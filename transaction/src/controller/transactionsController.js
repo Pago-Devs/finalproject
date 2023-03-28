9f02982d587a2e22878eaf6e6aa3b3ce288cf6ec
thayarasiqueira <thay.siqu@gmail.com>
2023-03-28 10:44:59 -0300

    fix: POST /v1/transaction

--- transaction/src/controller/transactionsController.js
+++ transaction/src/controller/transactionsController.js
@@ -21,25 +21,42 @@ class TransactionController {
 
   // eslint-disable-next-line consistent-return
   static createTransaction = async (req, res) => {
-    // const resultTransaction = await fetch('http://pagodevs-client:3001/v1/clients/verify')
-    //   .then((response) => response.json());
-    const resultTransaction = { id: '63d94cc7f8c08a1d745cb167', monthlyIncome: 4000 };
-
-    if (!resultTransaction) return res.status(404).send({ message: 'Dados Inválidos' });
+    const response = await fetch('http://pagodevs-client:3001/v1/clients/verify', {
+      method: 'GET',
+      body: JSON.stringify({
+        cardData: req.body.cardData,
+      }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    });
+    const resultTransaction = await response.json();
+    const { clientId, monthlyIncome, message } = resultTransaction;
+    // const resultTransaction = { id: '63d94cc7f8c08a1d745cb167', monthlyIncome: 4000 };
+    if (message === 'Dados inválidos') return res.status(422).send({ message: 'Dados Inválidos' });
 
     let status = '';
-    if ((resultTransaction.monthlyIncome * 0.5) > req.body.amount) {
+    if ((monthlyIncome * 0.5) <= req.body.amount) {
       status = 'Em análise';
-      // const resultAntiFraud = await fetch('http://pagodevs-client:3001/v1/clients/verify')
-      //   .then((response) => response.json());
-      console.log('testeee');
     } else {
       status = 'Aprovada';
     }
-    const transaction = new Transaction({ ...req.body, status, clientId: resultTransaction.id });
-    transaction.save((err, t) => {
+    const transaction = new Transaction({ ...req.body, status, clientId });
+    transaction.save(async (err, t) => {
       if (err) return res.status(500).send({ message: err.message });
       // eslint-disable-next-line no-underscore-dangle
+      if (status === 'Em análise') {
+        await fetch('http://pagodevs-antifraud:3003/v1/analise/client', {
+          method: 'POST',
+          body: JSON.stringify({
+            id: t.id,
+            clientId,
+          }),
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        });
+      }
       const result = { id: t.id, status };
       return res.status(201).json(result);
     });
