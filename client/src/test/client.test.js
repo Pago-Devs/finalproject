83af795e42821d1686865c9fa10ba90830395db1
escorsi <patrickescorsi@gmail.com>
2023-03-29 10:09:35 -0300

    feat: finalizing test implementation

--- client/src/test/client.test.js
+++ client/src/test/client.test.js
@@ -16,23 +16,79 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-let idResposta;
+describe('Testando routes', () => {
+  it('Deve retornar recurso selecionado', async () => {
+    await request(app)
+      .get('/')
+      .send({
+        titulo: 'Projeto Final',
+      })
+      .expect(200);
+  });
+});
+
 describe('GET em /v1/clients/verify', () => {
   it('Deve retornar recurso selecionado', async () => {
     await request(app)
       .get('/v1/clients/verify')
-      .set('Accept', 'application/json')
-      .expect('content-type', /json/)
+      .send({
+        name: 'Olnar T Silva',
+        numberCard: '5537464499559077',
+        expirationDate: '27/02/2024',
+        cvv: '554',
+      })
       .expect(200);
+  });
+  it('Deve retornar erro ao passar dados inválidos', async () => {
+    await request(app)
+      .get('/v1/clients/verify')
+      .send({
+        name: 'Olnar T Silva',
+        numberCard: '5537464499559077',
+        expirationDate: '27/02/2024',
+        cvv: '111',
+      })
+      .expect(400);
+  });
+  it('Deve retornar erro ao passar dados inválidos', async () => {
+    await request(app)
+      .get('/v1/clients/verify')
+      .send({
+        name: 'Olnar T Silva',
+        numberCard: '5537464499559077',
+        expirationDate: '27/02/2030',
+        cvv: '554',
+      })
+      .expect(400);
+  });
+  it('Deve retornar erro ao passar dados inválidos', async () => {
+    await request(app)
+      .get('/v1/clients/verify')
+      .send({
+        name: 'Olnar',
+        numberCard: '5537464499559077',
+        expirationDate: '27/02/2024',
+        cvv: '554',
+      })
+      .expect(400);
+  });
+  it('Deve nao adicionar nada ao passar o body vazio', async () => {
+    await request(app)
+      .post('/v1/clients/verify')
+      .send({})
+      .expect(404);
   });
 });
 
 describe('GET em /v1/clients/:id', () => {
   it('Deve retornar recurso selecionado', async () => {
     await request(app)
-      .get(`/v1/clients/${idResposta}`)
-      .set('Accept', 'application/json')
-      .expect('content-type', /json/)
+      .get('/v1/clients/6422dbf3a320cb145d974f2d')
       .expect(200);
+  });
+  it('Deve retornar erro ao passar dados inválidos', async () => {
+    await request(app)
+      .post('/v1/clients/111111')
+      .expect(404);
   });
 });
